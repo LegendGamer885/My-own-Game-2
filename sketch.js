103d4adabfcbcc72d488cbe57d0a748ccfa163d6
Legend <85244055+LegendGamer885@users.noreply.github.com>
2021-09-05 17:00:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ function draw() {
 
     if (meteorGroup.isTouching(invisibleEarth)) {
       life = life - 1;
-      meteorGroup.destroyEach();
+      meteorGroup[0].destroyEach();
     }
 
     if(alienGroup.isTouching(jet)){
